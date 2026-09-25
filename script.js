754cdd5c57d5e040e07751112ccae89919d75211
FABIO ANDERSON DA SILVA RODRIGUES <fabio.rodrigues005@cs.udf.edu.br>
2026-09-25 09:17:10 -0300

    fix: corrige calculo da media

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let media = 8.0;
+function calcularMedia() {
+    let nota1 = Number(document.getElementById("nota1").value);
+    let nota2 = Number(document.getElementById("nota2").value);
+    let nota3 = Number(document.getElementById("nota3").value);
 
-if (media >= 6) {
-    document.getElementById("resultado").innerHTML =
-        "Média: " + media.toFixed(1) + "<br>APROVADO!";
-} else {
-    document.getElementById("resultado").innerHTML =
-        "Média: " + media.toFixed(1) + "<br>REPROVADO!";
+    let media = (nota1 + nota2 + nota3) / 3;
+
+    if (media >= 6) {
+        document.getElementById("resultado").innerHTML =
+            "Média: " + media.toFixed(1) + "<br>APROVADO!";
+    } else {
+        document.getElementById("resultado").innerHTML =
+            "Média: " + media.toFixed(1) + "<br>REPROVADO!";
+    }
 }
+
+document.getElementById("calcular").addEventListener("click", calcularMedia);
